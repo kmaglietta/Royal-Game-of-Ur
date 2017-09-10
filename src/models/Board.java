78213dc0b07cab6417aabe0f6f6afc3f62db1b8b
Kyle <Kyle@DESKTOP-A3C8JGE>
2017-09-10 10:54:00 -0400

    Finished creating the classic board

--- src/models/Board.java
+++ src/models/Board.java
@@ -21,12 +21,15 @@ public class Board {
 		Cell _2_nextCell = new Cell();
 		Cell _0_nextCell = new Cell();
 		
+		Cell _1_tempCell;
+		Cell _2_tempCell; 
+		
 		this._1_stattingCell = _1_nextCell;
 		this._2_startingCell = _2_nextCell;
 		
 		for (int i = 0; i <= 2; i++){
-			Cell _1_tempCell = new Cell(i == 2);
-			Cell _2_tempCell = new Cell(i == 2);
+			_1_tempCell = new Cell(i == 2);
+			_2_tempCell = new Cell(i == 2);
 			
 			_1_nextCell.setNextCell(_1_tempCell);
 			_2_nextCell.setNextCell(_2_tempCell);
@@ -39,11 +42,35 @@ public class Board {
 		_2_nextCell.setNextCell(_0_nextCell);
 		
 		for (int i = 0; i <= 6; i++) {
-			Cell _0_tempCell = new Cell(i == 2);
+			Cell _0_tempCell;
+			if (i == 6) {
+				_0_tempCell = new Cell2Way(i == 2);
+			}
+			else {
+				_0_tempCell = new Cell(i == 2);
+			}
 			
 			_0_nextCell.setNextCell(_0_tempCell);
 			
 			_0_nextCell = _0_tempCell;
 		}
+		
+
+		_1_nextCell = new Cell();
+		_2_nextCell = new Cell();
+		((Cell2Way) _0_nextCell).setNextCells(_1_nextCell, _2_nextCell);
+		
+		for (int i = 0; i <= 1; i++) {
+			_1_tempCell = new Cell(i == 1);
+			_2_tempCell = new Cell(i == 1);
+			
+			_1_nextCell.setNextCell(_1_tempCell);
+			_2_nextCell.setNextCell(_2_tempCell);
+			
+			_1_nextCell = _1_tempCell;
+			_2_nextCell = _2_tempCell;
+		}
+		
+		// Classic board made
 	}
 }
